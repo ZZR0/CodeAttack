944f9dac1e1d427212c0f8d6223a339695fa9853
ZZR0 <>
2022-01-08 22:19:54 +0800

    Fix Script.

--- script/clone_detection_bigclonebench.sh
+++ script/clone_detection_bigclonebench.sh
@@ -6,6 +6,7 @@ function run() {
     model=$1
     mode_path=$2
     tokenizer_path=$3
+    re=$4
 
     python adv_clone_detection_bigclonebench.py \
             --test_data_file ./dataset/clone_detection_bigclonebench/transforms.Replace/adv_test.jsonl \
@@ -15,11 +16,13 @@ function run() {
             --save_dir ./saved_models/ \
             --task clone_bcb \
             --num_examples -1 \
-            2>&1 | tee ./saved_models/$model/clone_detection_bigclonebench.log
+            --recipe $re \
+            2>&1 | tee ./saved_models/$model/clone_detection_bigclonebench_$re.log
 }
 
 function codebert() {
-    run codebert microsoft/codebert-base roberta-base
+    run codebert microsoft/codebert-base roberta-base textfooler
+    run codebert microsoft/codebert-base roberta-base random
 }
 
 for model in codebert
